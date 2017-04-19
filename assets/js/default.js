d615dc8be99970f7ee9f3e39512d819554066672
Maxime Bret <maxime.bret.ext@boursorama.fr>
2017-04-19 18:09:02 +0200

    add map

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,12 +1,12 @@
 $(window).load(function() {
   // Animate loader off screen
-  $(".se-pre-con").fadeOut("slow");;
+  $(".se-pre-con").fadeOut("slow");
 });
 $(document).ready(function(){
   $('body').scrollspy({
     target: '.navbar-fixed-top',
     offset: 40
-  })
+  });
   $('a.scrollto').bind('click', function(event) {
       var $anchor = $(this);
       $('html, body').stop().animate({
@@ -17,21 +17,6 @@ $(document).ready(function(){
 
   //parallax
   $('.parallax-window').parallax();
-});
-
-// RSVP Form - jquery validation
-$("#rsvpForm").validate({
-  rules: {
-    guestName: {
-      required: true
-    },
-    guestEmail: {
-      required: true
-    },
-    guestMessage: {
-      required: false
-    }
-  }
 });
 
 // wow
@@ -63,14 +48,155 @@ if ($('#back-to-top').length) {
 }
 
 // Gallery
-document.getElementById('gallery').onclick = function (event) {
-    event = event || window.event;
-    var target = event.target || event.srcElement,
-        link = target.src ? target.parentNode : target,
-        options = {index: link, event: event},
-        links = this.getElementsByTagName('a');
-   blueimp.Gallery(links, options);   
-};
+// document.getElementById('gallery').onclick = function (event) {
+//     event = event || window.event;
+//     var target = event.target || event.srcElement,
+//         link = target.src ? target.parentNode : target,
+//         options = {index: link, event: event},
+//         links = this.getElementsByTagName('a');
+//    blueimp.Gallery(links, options);
+// };
+
+function is_touch_device() {
+    return !!('ontouchstart' in window) // works on most browsers
+        || !!('onmsgesturechange' in window); // works on ie10
+}
+
+function ToggleDrag(controlDiv, map) {
+
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.margin = '10px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Cliquez pour activer ou désactiver le contrôle de la map';
+    controlDiv.appendChild(controlUI);
+
+    var a = document.createElement("div");
+    a.className += "gm-style-mtc";
+    controlUI.appendChild(a);
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontSize = '11px';
+    controlText.style.padding = '8px';
+    controlText.style.boxShadow = "0px 1px 4px -1px rgba(0, 0, 0, 0.298039)";
+    controlText.innerHTML = map.draggable ? 'Désactiver le contrôle de map' : "Activer le contrôle de map";
+    a.appendChild(controlText);
+
+    controlUI.addEventListener('click', function() {
+        map.setOptions( {
+            draggable: !map.draggable
+        });
+        controlText.innerHTML = map.draggable ? 'Désactiver le contrôle de map' : "Activer le contrôle de map";
+    });
+}
+
+function initMap() {
+    // https://developers.google.com/maps/documentation/javascript/reference#Marker
+    // https://developers.google.com/maps/documentation/javascript/infowindows#open
+    var coordNiderviller = {lat: 48.7121523, lng: 7.1071901};
+    var coordStrasbourg = {lat: 48.584922, lng: 7.7750307};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: coordStrasbourg,
+        scrollwheel: false,
+        draggable: !is_touch_device()
+    });
+
+    // Create the DIV to hold the control and call the CenterControl()
+    // constructor passing in this DIV.
+    var toggleDagControlDiv = document.createElement('div');
+    var toggleDagControl = new ToggleDrag(toggleDagControlDiv, map);
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDagControlDiv);
+
+    // Niderviller address (ceremonie + brunch)
+    var infowindow = new google.maps.InfoWindow({
+        content: '' +
+        '<div class="note">Vin d\'honneur et réception</div>' +
+        '<div class="">(Brunch le dimanche)</div>' +
+        '<h4 class="map-title script">Restaurant M</h4>' +
+        '<div class="address">' +
+        '<span class="region">7 place du Général de Gaulle</span><br>' +
+        '<span class="postal-code">57565</span><br>' +
+        '<span class="city-name">Niderviller</span><br>' +
+        '</div>'
+    });
+    var marker = new google.maps.Marker({
+        position: coordNiderviller,
+        map: map,
+        title: 'Vin d\'honneur et réception'
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+        infowindow2.close();
+    });
+
+    // Mariage
+    var infowindow2 = new google.maps.InfoWindow({
+        content: '' +
+        '<div class="note">Cérémonie</div>' +
+        '<h4 class="map-title script">Salle des mariages, hôtel de ville</h4>' +
+        '<div class="address">' +
+        '<span class="region">Place Broglie</span><br>' +
+        '<span class="postal-code">67000</span><br>' +
+        '<span class="city-name">Strasbourg</span><br>' +
+        '</div>'
+    });
+    var marker2 = new google.maps.Marker({
+        position: coordStrasbourg,
+        map: map,
+        title: 'Cérémonie'
+    });
+    infowindow2.open(map, marker2);
+    marker2.addListener('click', function() {
+        infowindow.close();
+        infowindow2.open(map, marker2);
+    });
+
+    var markers = {
+        niderviller: {
+            marker: marker,
+            infoWindow: infowindow
+        },
+        strasbourg: {
+            marker: marker2,
+            infoWindow: infowindow2
+        }
+    };
+
+    // This listener allow opening correct marker info on click
+    // by specifying [data] value
+    $('[data-target-map-marker]').on('click', function(){
+        // close all previous info
+        for(var tmp in markers){
+            markers[tmp].infoWindow.close();
+        }
+        // open info for current marker
+        var marker = markers[$(this).data('targetMapMarker')];
+        marker.infoWindow.open(map, marker.marker);
+    });
+}
+
+// RSVP Form - jquery validation
+$("#rsvpForm").validate({
+    rules: {
+        guestName: {
+            required: true
+        },
+        guestEmail: {
+            required: true
+        },
+        guestMessage: {
+            required: false
+        }
+    }
+});
 
 $('#rsvp-form-submit').on('click',function(){
   if($("#rsvpForm").valid()) {
